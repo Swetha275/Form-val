a753ce5e76a95b1f47484b6141bc36ac0d101184
Swetha275 <swethikiswe@gmail.com>
2023-04-10 13:26:25 +0530

    final

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express = require('express');
 var bodyParser = require('body-parser')
-const app = express();
 const mysql = require("mysql");
-
 var cors = require("cors");
+
+const app = express();
 app.use(cors());
 app.use(express.json());
 var urlencodedParser = bodyParser.urlencoded({ extended: false })
@@ -11,15 +11,34 @@ var urlencodedParser = bodyParser.urlencoded({ extended: false })
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
+app.get('/display.html', (req, res) => {
+  res.sendFile(__dirname + '/display.html');
+});
+
+app.get('/content', (req, res) => {
+  connection.query('SELECT * FROM  formjstable', function (error, results, fields) {
+    if (error) throw error;
+    console.log(results);
+    res.send(results);
+  });
+});
     
 app.post('/', urlencodedParser, (req, res) => {
 
     const data = {
-        fname: req.body.fname,
-        lname: req.body.lname,
-        email: req.body.email,
+        fname    : req.body.fname,
+        lname    : req.body.lname,
+        email    : req.body.email,
+        dob     : req.body.dob     ,
+        gender  : req.body.gender ,
+        adl1    : req.body.adl1   ,
+        adl2    : req.body.adl2   ,
+        city    : req.body.city   ,
+        state   : req.body.state  ,
+        country : req.body.country,
+        zipcode : req.body.zipcode,
       };
-      
+
     console.log('Got body:', req.body);
     connection.query('INSERT INTO formjstable SET ?', data, function (error, results, fields) {
         if (error) throw error;
